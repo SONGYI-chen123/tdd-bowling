5bc4a3cb2a0aa1d3fc5f816b59368833ab8d95d6
SONGYI-chen123 <song1577660501@163.com>
2020-09-14 16:51:57 +0800

    should return 0 when no knockout

---
+++ src/test/java/BowlingGame.java
@@ -0,0 +1,5 @@
+public class BowlingGame {
+public String scoring_rules(String knockout_num ){
+    return "0";
+}
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BowlingGameTest {
+    public void should_return_0_when_No_knockout(){
+        BowlingGame bowlingGame = new BowlingGame();
+
+        String knockout_num = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+        String result = bowlingGame.scoring_rules(knockout_num);
+        assertEquals("0",result);
+    }
 }
