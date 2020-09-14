f6b29b0c5fac8687b8af72c38d143b3547113bb2
SONGYI-chen123 <song1577660501@163.com>
2020-09-14 18:34:35 +0800

    should return 300 when all strike

--- src/test/java/BowlingGame.java
+++ src/test/java/BowlingGame.java
@@ -1,5 +1,28 @@
 public class BowlingGame {
-public String scoring_rules(String knockout_num ){
-    return "0";
+public int scoring_rules(String knockout_num ){
+
+    int score_num =0;
+
+    String[] knockoutArr = knockout_num.split(",");
+    for(int i=0 ;i<knockoutArr.length-2;){
+        int single_num =0;
+        if(knockoutArr[i].equals("X")){
+            single_num = isstrike(knockoutArr[i])+isstrike(knockoutArr[i+1])+isstrike(knockoutArr[i+2]);
+           score_num +=single_num;
+           i++;
+        }
+
+    }
+
+    return score_num;
+}
+
+public int isstrike(String single_num){
+    if(single_num.equals("X")){
+        return 10;
+    }else if(!(single_num.equals("X") || single_num.equals("/"))){
+        return Integer.parseInt(single_num);
+    }
+    return 0;
 }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -6,8 +6,16 @@ public class BowlingGameTest {
     public void should_return_0_when_No_knockout(){
         BowlingGame bowlingGame = new BowlingGame();
 
-        String knockout_num = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
-        String result = bowlingGame.scoring_rules(knockout_num);
-        assertEquals("0",result);
+        String knockout_num = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+        int result = bowlingGame.scoring_rules(knockout_num);
+        assertEquals(0,result);
+    }
+
+    public void should_return_330_when_all_strike(){
+        BowlingGame bowlingGame = new BowlingGame();
+
+        String knockout_num = "X，X，X，X，X，X，X，X，X，X,10,10";
+        int result = bowlingGame.scoring_rules(knockout_num);
+        assertEquals(300,result);
     }
 }
