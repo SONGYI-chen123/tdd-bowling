4320c57660d0bc644f0ba0273dafae0cf1d9157a
SONGYI-chen123 <song1577660501@163.com>
2020-09-20 21:38:00 +0800

    add a test

--- src/test/java/BowlingGame.java
+++ src/test/java/BowlingGame.java
@@ -1,38 +1,37 @@
 public class BowlingGame {
-public int scoring_rules(String knockout_num ){
+    public int scoring_rules(String knockout_num) {
 
-    int score =0;
-    String[] knockoutArr = knockout_num.split(",");
-    int thrownum = 19;
-    for(int i=0 ;i<thrownum;){
-        int single_num =0;
-        if(knockoutArr[i].equals("X")){
-            single_num = isstrike(knockoutArr[i])+isstrike(knockoutArr[i+1])+isstrike(knockoutArr[i+2]);
-           score +=single_num;
-           thrownum = thrownum-1;
-           i++;
+        int score = 0;
+        String[] knockoutArr = knockout_num.split(",");
+        int thrownum = 19;
+        for (int i = 0; i < thrownum; ) {
+            int single_num = 0;
+            if (knockoutArr[i].equals("X")) {
+                single_num = isstrike(knockoutArr[i]) + isstrike(knockoutArr[i + 1]) + isstrike(knockoutArr[i + 2]);
+                score += single_num;
+                thrownum = thrownum - 1;
+                i++;
+            } else if (knockoutArr[i + 1].equals("/")) {
+                single_num = isstrike(knockoutArr[i + 1]) + isstrike(knockoutArr[i + 2]);
+                score += single_num;
+                i = i + 2;
+            } else {
+                single_num = isstrike(knockoutArr[i]) + isstrike(knockoutArr[i + 1]);
+                score += single_num;
+                i = i + 2;
+            }
         }
-        else if(knockoutArr[i+1].equals("/")){
-            single_num = isstrike(knockoutArr[i+1])+isstrike(knockoutArr[i+2]);
-            score +=single_num;
-            i=i+2;
-        }else{
-            single_num = isstrike(knockoutArr[i])+isstrike(knockoutArr[i+1]);
-            score +=single_num;
-            i=i+2;
-        }
+
+        return score;
     }
 
-    return score;
-}
-
-public int isstrike(String single_num){
-    if(single_num.equals("X")){
-        return 10;
-    }else if(single_num.equals("/")){
-        return 10;
-    }else{
-        return Integer.parseInt(single_num);
+    public int isstrike(String single_num) {
+        if (single_num.equals("X")) {
+            return 10;
+        } else if (single_num.equals("/")) {
+            return 10;
+        } else {
+            return Integer.parseInt(single_num);
+        }
     }
-}
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -4,30 +4,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BowlingGameTest {
     @Test
-    public void should_return_0_when_No_knockout(){
+    public void should_return_0_when_No_knockout() {
         BowlingGame bowlingGame = new BowlingGame();
 
         String knockout_num = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
         int result = bowlingGame.scoring_rules(knockout_num);
-        assertEquals(0,result);
+        assertEquals(0, result);
     }
 
     @Test
-    public void should_return_300_when_all_strike(){
+    public void should_return_300_when_all_strike() {
         BowlingGame bowlingGame = new BowlingGame();
 
         String knockout_num = "X,X,X,X,X,X,X,X,X,X,10,10";
         int result = bowlingGame.scoring_rules(knockout_num);
-        assertEquals(300,result);
+        assertEquals(300, result);
     }
 
     @Test
-    public void should_return_score_num_when_not_all_strike_nor_no_knockout(){
+    public void should_return_score_num_when_not_all_strike_nor_no_knockout() {
         BowlingGame bowlingGame = new BowlingGame();
 
         String knockout_num = "3,2,5,/,9,/,1,6,X,0,5,6,2,3,4,5,3,7,/,4";
         int result = bowlingGame.scoring_rules(knockout_num);
-        assertEquals(99,result);
+        assertEquals(99, result);
     }
+
+    @Test
+    public void should_return_score_num_when_no_strike(){
+        BowlingGame bowlingGame = new BowlingGame();
+
+        String knockout_num = "0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9";
+        int result = bowlingGame.scoring_rules(knockout_num);
+        assertEquals(90, result);
+    }
+
+    
 
 }
